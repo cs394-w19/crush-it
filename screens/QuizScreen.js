fb59adbd53db4d45cb6bceafd0667e7a2a6d9f7d
Kelly Wachtel <kwachtel24@gmail.com>
2019-02-25 18:25:31 -0600

    added photo to header

--- screens/QuizScreen.js
+++ screens/QuizScreen.js
@@ -32,6 +32,12 @@ export default class QuizScreen extends React.Component {
     return {
       title: "Quiz",
       tabBarVisible: false,
+      headerStyle: {height: 65},
+      headerLeft: (
+        <Image  
+          source={require('../assets/images/logos/CrushItLOGOsmall.png')}
+        />
+      ),
     };
   };
 
