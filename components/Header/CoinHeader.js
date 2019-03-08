3748f5cf199acf6dcb53288bb8ffe5f5fefdd48e
dmcgrory20 <danielmcgrory2020@u.northwestern.edu>
2019-03-07 22:50:02 -0600

    fixed Partner Page error

--- components/Header/CoinHeader.js
+++ components/Header/CoinHeader.js
@@ -13,14 +13,14 @@ export default class CoinHeader extends React.Component {
       <TouchableOpacity style={styles.coinsTotal} onPress={() => this.props.navigation.navigate("Partners", 
       {
         points: this.props.navigation.getParam("points", 0),
-        availabilities: navigation.getParam("availabilities", [
+        availabilities: this.props.navigation.getParam("availabilities", [
           [true, false, false],
           [false, false, false],
           [false, false, false],
           [false, false, false],
           [false, false, false],
         ]),
-        categoryIndex: navigation.getParam("categoryIndex", 0)
+        categoryIndex: this.props.navigation.getParam("categoryIndex", 0)
       })}>
         <View style={{ flex: 1 }}>
           <Image
