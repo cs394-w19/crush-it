6df5040e8c16fa851acc5a552ab5135926c32e88
Varun Ganglani <varunganglani2019@u.northwestern.edu>
2019-03-12 15:31:14 -0500

    3

--- assets/quiz_data.js
+++ assets/quiz_data.js
@@ -403,128 +403,178 @@ export default (quiz_data = [
     quizLevel: 3,
     questions: [
       {
-        question:
-          "Suppose you buy a $100 shirt on a credit card with 30% APR. If you don’t make any payments, how much do you owe after a year?\n\nA. Less than $120\nB. About $130\nC. Wait. Pay?",
-        answerChoices: [
-          {
-            buttonOrder: "1",
-            answerText: "A",
-            isCorrect: false
-          },
-          {
-            buttonOrder: "0",
-            answerText: "B",
-            isCorrect: true
-          },
-          {
-            buttonOrder: "1",
-            answerText: "C",
-            isCorrect: false
-          }
-        ]
-      },
-      {
-        image: require("./images/credit-card-debt/dog.gif"),
+        header: "Level 3",
+        statement:
+          "Now let’s say your credit card has been stolen…\n\nThe thief charged $10,000!",
+        image: require("./images/credit-card-debt/shit.png"),  
         answerChoices: [
           {
             buttonOrder: "2",
-            answerText: "Next",
-            isCorrect: true
-          }
-        ]
-      },
-      {
-        statement: "",
-        question:
-          " If inflation goes up, what’s going to happen to the interest rate of your credit card?",
-        answerChoices: [
-          {
-            buttonOrder: "0",
-            answerText: "It goes up",
-            isCorrect: true
+            answerText: "Sh*t... Now what?",
+            isCorrect: true,
+            emoji: ""
           },
-          {
-            buttonOrder: "1",
-            answerText: "It goes down",
-            isCorrect: false
-          }
-        ]
-      },
-      {
-        image: require("./images/credit-card-debt/im-rich.gif"),
-        answerChoices: [
-          {
-            buttonOrder: "2",
-            answerText: "Next",
-            isCorrect: true
-          }
-        ]
-      },
-      {
-        statement: "",
-        question:
-          "Suppose you have a $1000 balance on a credit card with 30% APR. If you don’t make any payments, how long will it take for that balance to double?\n\nA. Less than 3 years\nB. More than 3 years\nC. Wait. Payment?",
-        answerChoices: [
-          {
-            buttonOrder: "0",
-            answerText: "A",
-            isCorrect: true
-          },
-          {
-            buttonOrder: "1",
-            answerText: "B",
-            isCorrect: false
-          },
-          {
-            buttonOrder: "1",
-            answerText: "C",
-            isCorrect: false
-          }
-        ]
-      },
-      {
-        image: require("./images/credit-card-debt/poor.gif"),
-        answerChoices: [
-          {
-            buttonOrder: "2",
-            answerText: "Next",
-            isCorrect: true
-          }
         ]
       },
       {
         statement:
-          "Suppose you have $100 in a savings account earning 2% interest a year, and a credit card with a balance of $100 and 20% APR. What is the best thing to do?",
+          "How much are you liable for?",
         question:
-          "A. Pay off the credit card!\nB. Don’t do a thing and get all of that interest!\nC. IDK",
+          "A.  Nothing!\n\nB.  $50 until card reported stolen\n\nC.  The full $10,000\n\nD.  A & B",
         answerChoices: [
           {
-            buttonOrder: "0",
+            buttonOrder: "1",
             answerText: "A",
-            isCorrect: true
+            isCorrect: false,
+            emoji: ""
           },
           {
             buttonOrder: "1",
             answerText: "B",
-            isCorrect: false
+            isCorrect: false,
+            emoji: ""
           },
           {
             buttonOrder: "1",
             answerText: "C",
-            isCorrect: false
+            isCorrect: false,
+            emoji: ""
+          },
+          {
+            buttonOrder: "0",
+            answerText: "D",
+            isCorrect: true,
+            emoji: ""
+          }
+         ]
+      },
+      {
+        statement: "If the thief has already started holiday shopping on your dime, you may be liable for up to $50.\n\nEasily report the theft to your CC company via phone or online.\n\nThen you’re off the hook!",
+        answerChoices: [
+          {
+            buttonOrder: "2",
+            answerText: "Whew!",
+            isCorrect: true,
+            emoji:""
+          },
+        ]
+      },
+      {
+        statement: "You’re also protected from being taken advantage of.",
+        question:
+          "Can a credit card issuer, such as Chase or Discover, change your interest rate at any time without notice?",
+        answerChoices: [
+          {
+            buttonOrder: "0",
+            answerText: "Yes",
+            isCorrect: true,
+            emoji:""
+          },
+          {
+            buttonOrder: "1",
+            answerText: "No",
+            isCorrect: false,
+            emoji:""
+          },
+        ]
+      },
+      {
+        statement:
+          "Most of the time, issuers don’t need to provide notice.",
+        question:
+          "Which one situation does require notice?\n\nA.  Paid ≥ 60 days late\nB.  Fixed APR\nC.  Promotion expired\nD.  Variable interest",
+        answerChoices: [
+          {
+            buttonOrder: "1",
+            answerText: "A",
+            isCorrect: false,
+            emoji: ""
+          },
+          {
+            buttonOrder: "0",
+            answerText: "B",
+            isCorrect: true,
+            emoji: ""
+          },
+          {
+            buttonOrder: "1",
+            answerText: "C",
+            isCorrect: false,
+            emoji: ""
+          },
+          {
+            buttonOrder: "1",
+            answerText: "D",
+            isCorrect: false,
+            emoji: ""
+          }
+         ]
+      },
+      {
+        statement:
+          "Because fixed APRs are set-in-stone, issuers must provide 45-day notice prior to any changes.\n\nThe Captain is looking out for you.",
+        image: require("./images/credit-card-debt/cap.png"),
+        answerChoices: [
+          {
+            buttonOrder: "2",
+            answerText: "Thanks Cap!",
+            isCorrect: true,
+            emoji:""
           }
         ]
       },
       {
-        image: require("./images/credit-card-debt/bear.gif"),
+        header: "PRO TIP",
+        statement:
+          "Reading the fine print of a contract can be a total snooze.\n\nHowever, it can help you avoid late fees and save you money in the long run.\n\nWho doesn’t love money?",
         answerChoices: [
           {
             buttonOrder: "2",
-            answerText: "Next",
-            isCorrect: true
-          }
+            answerText: "OK, I'll look",
+            isCorrect: true,
+            emoji: ""
+          },
         ]
-      }
+      },
+      {
+        statement:
+          "Speaking of fine print…\n\nIf you’re an authorized user on your parents’ account, who is liable if you can’t pay your charges?",
+        question:
+          "A.  You are\n\nB.  Your parents\n\nC.  No one if you're under 21",
+        answerChoices: [
+          {
+            buttonOrder: "1",
+            answerText: "A",
+            isCorrect: false,
+            emoji: ""
+          },
+          {
+            buttonOrder: "0",
+            answerText: "B",
+            isCorrect: true,
+            emoji: ""
+          },
+          {
+            buttonOrder: "1",
+            answerText: "C",
+            isCorrect: false,
+            emoji: ""
+          },
+        ]
+      },
+      {
+        statement:
+          "The risk is all theirs.\n\nKeep in mind… they likely know where you live…",
+        image: require("./images/credit-card-debt/im-watching-you.gif"),
+        answerChoices: [
+          {
+            buttonOrder: "2",
+            answerText: "I'll pay! I swear!",
+            isCorrect: true,
+            emoji: ""
+          },
+        ]
+      },
     ]
   }
 ]);
